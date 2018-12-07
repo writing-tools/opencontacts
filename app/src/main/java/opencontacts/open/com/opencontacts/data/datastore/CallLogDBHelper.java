53b7875494e10daf1c4cd3a7165525ab2fad3562
Sultanahamer <sultanahamer@gmail.com>
2018-12-07 21:50:10 +0530

    Removed dynamic permission asking todo as it was implemented  already

--- app/src/main/java/opencontacts/open/com/opencontacts/data/datastore/CallLogDBHelper.java
+++ app/src/main/java/opencontacts/open/com/opencontacts/data/datastore/CallLogDBHelper.java
@@ -50,13 +50,6 @@ class CallLogDBHelper {
 
     private List<CallLogEntry> getRecentCallLogEntries(final Context context){
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             Handler mainHandler = new Handler(context.getMainLooper());
 
             mainHandler.post(() -> Toast.makeText(context, R.string.grant_read_call_logs_permission, Toast.LENGTH_LONG).show());

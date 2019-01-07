412d8054748a66bfc8e0af46106534ee043a28a3
Sultanahamer <sultanahamer@gmail.com>
2019-01-07 22:22:04 +0530

    Oreo will now get notifications for missed calls
    created notification channel so that oreo and above devices get missed
    call notifications

--- app/src/main/java/opencontacts/open/com/opencontacts/OpenContactsApplication.java
+++ app/src/main/java/opencontacts/open/com/opencontacts/OpenContactsApplication.java
@@ -1,13 +1,33 @@
 package opencontacts.open.com.opencontacts;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
 import opencontacts.open.com.opencontacts.data.datastore.CallLogDataStore;
 import opencontacts.open.com.opencontacts.data.datastore.ContactsDataStore;
 
+import static android.app.NotificationManager.IMPORTANCE_HIGH;
+
 public class OpenContactsApplication extends com.orm.SugarApp{
+
+    public static final String MISSED_CALLS_CHANEL_ID = "6477";
+
     @Override
     public void onCreate() {
         super.onCreate();
         ContactsDataStore.init();
         CallLogDataStore.init(getApplicationContext());
+        createNotificationChannels();
+    }
+
+    private void createNotificationChannels() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(MISSED_CALLS_CHANEL_ID, getString(R.string.notification_channel_missed_calls), IMPORTANCE_HIGH);
+            channel.setDescription(getString(R.string.notification_channel_missed_calls_description));
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
     }
 }

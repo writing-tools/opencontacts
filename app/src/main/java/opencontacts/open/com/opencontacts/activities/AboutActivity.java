be6532ffa5c64bea451860548d08421f941e007c
Sultanahamer <sultanahamer@gmail.com>
2019-01-09 07:08:53 +0530

    About page now also tells version number of app

--- app/src/main/java/opencontacts/open/com/opencontacts/activities/AboutActivity.java
+++ app/src/main/java/opencontacts/open/com/opencontacts/activities/AboutActivity.java
@@ -1,10 +1,21 @@
 package opencontacts.open.com.opencontacts.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatTextView;
+
+import opencontacts.open.com.opencontacts.BuildConfig;
 import opencontacts.open.com.opencontacts.R;
 
 public class AboutActivity extends AppBaseActivity{
     @Override
     int getLayoutResource() {
         return R.layout.activity_about;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ((AppCompatTextView)findViewById(R.id.version)).setText(BuildConfig.VERSION_NAME);
     }
 }

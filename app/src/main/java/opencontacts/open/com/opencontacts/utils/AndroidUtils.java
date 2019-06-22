e5b22a0e729a82a1f29fed8f64acf538805d41f1
Sultanahamer <sultanahamer@gmail.com>
2019-06-22 17:41:08 +0530

    App can now handle USSDs

--- app/src/main/java/opencontacts/open/com/opencontacts/utils/AndroidUtils.java
+++ app/src/main/java/opencontacts/open/com/opencontacts/utils/AndroidUtils.java
@@ -120,16 +120,7 @@ public class AndroidUtils {
 
     @NonNull
     public static Intent getCallIntent(String number, Context context) {
-        // Fix phone call with USSD code problems (sultanahamer/OpenContacts issue #81)
-        String encodedNumber = "";
-        for(char c : number.toCharArray()) {
-            if(c == '#')
-                encodedNumber += Uri.encode(String.valueOf(c));
-            else
-                encodedNumber += c;
-        }
-
-        Uri numberUri = Uri.parse("tel:" + encodedNumber);
+        Uri numberUri = Uri.parse("tel:" + Uri.encode(number));
         Intent callIntent;
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE) != PERMISSION_GRANTED) {
             callIntent = new Intent(Intent.ACTION_DIAL, numberUri);

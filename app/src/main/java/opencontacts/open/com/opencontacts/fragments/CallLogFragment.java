dc8b76d8cce98d70fbb2c5f20f94ff0024640e31
Sultan ahamar <sultanm@sultanm.local>
2020-10-19 07:30:49 +0530

    Auto shifting tabs is letting unselects happen even before stuff is rendered

--- app/src/main/java/opencontacts/open/com/opencontacts/fragments/CallLogFragment.java
+++ app/src/main/java/opencontacts/open/com/opencontacts/fragments/CallLogFragment.java
@@ -42,6 +42,7 @@ public class CallLogFragment extends AppBaseFragment implements SelectableTab {
 
     @Override
     public void onUnSelect() {
+        if(callLogListView == null) return;
         callLogListView.exitSelectionMode();
     }
 

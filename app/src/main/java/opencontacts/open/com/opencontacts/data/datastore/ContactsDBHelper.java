5e7c9cd608dad12e469a15a22560d8792b3457c1
vag_ssd <vag@ssd.com>
2022-10-07 08:24:37 +0000

    Fix robo calls being assigned random contact

--- app/src/main/java/opencontacts/open/com/opencontacts/data/datastore/ContactsDBHelper.java
+++ app/src/main/java/opencontacts/open/com/opencontacts/data/datastore/ContactsDBHelper.java
@@ -77,6 +77,7 @@ public class ContactsDBHelper {
     }
 
     static Contact getContactFromDB(String phoneNumber) {
+        if (TextUtils.isEmpty(phoneNumber)) return null;
         String searchablePhoneNumber = getSearchablePhoneNumber(phoneNumber);
         if (searchablePhoneNumber == null) return null;
         List<PhoneNumber> matchingPhoneNumbers = getMatchingNumbers(searchablePhoneNumber);
